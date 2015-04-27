9a20042854e0d9fc9d1e9b72e3a526a0cd405e17
ramfa <ramosgustavofacundo@gmail.com>
2015-04-27 01:14:47 -0300

    Se crea admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin=Admin.create!(:email=>'test@test.com',:password=>'password')
