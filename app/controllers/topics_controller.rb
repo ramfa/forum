02f086d5139a960334e61380f6c0e3ff9625ff91
ramfa <ramosgustavofacundo@gmail.com>
2015-04-27 01:10:40 -0300

    se modifico opciones para el admin

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,6 @@
 class TopicsController < ApplicationController
 before_action :find_topic, only: [:show, :edit, :update, :destroy]
-before_action :authenticate_admin!, only: [:new]
+before_action :authenticate_admin!, only: [:new, :create]
 
 def index
   @topics=Topic.all
